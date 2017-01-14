ffdaef420970b0aa16424d0d905d10630090ff1e
megha dubey <dubeymegha14@gmail.com>
2017-01-13 17:32:53 -0800

    Star pattern basic program

---
+++ Star_pattern_5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class Star_pattern_5
+    {
+        static void Main(string[] args)
+        {
+            int n=1;
+          for(int i=0;i<7;i++)
+          {
+              for(int j=1;j<=i+1;j++)
+              {
+                  Console.Write(j);
+              }
+              for (int k = n; k < 7;k++ )
+              {
+                  Console.Write("*");
+              }
+                  Console.WriteLine();
+                  n++;
+          }
+          Console.ReadKey();
+        }
+    }
+}
