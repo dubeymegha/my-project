837aa7b02a9a478117c10b47de31c84a6e38c7ba
megha dubey <dubeymegha14@gmail.com>
2017-01-18 13:19:10 -0800

    print odd no using while

---
+++ print_oddno_using_while_10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class print_oddno_using_while_10
+    {
+        static void Main(string[] args)
+        {
+            int n = 15;
+            while (n >= 7)
+            {
+                if (n % 2 != 0)
+                {
+                    Console.WriteLine(n);
+                }
+                n--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
