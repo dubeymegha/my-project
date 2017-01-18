53146d86d29ed97a0de2d12bcd223c112edc758a
megha dubey <dubeymegha14@gmail.com>
2017-01-18 12:13:19 -0800

    Simple_find_noispositiveornegative

---
+++ find_pisotiveornegative_5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class find_pisotiveornegative_5
+    {
+        static void Main5(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int n = int.Parse(Console.ReadLine());
+            if(n>=0)
+                Console.WriteLine("no is positive");
+            else
+                Console.WriteLine("no is negative");
+            Console.ReadKey();
+        }
+    }
+}
