5956caaea0dd029cd20479b955f2404c0015b8e5
megha dubey <dubeymegha14@gmail.com>
2017-01-18 12:10:17 -0800

    Simple_product_calculation

---
+++ Product_calculation_6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class Product_calculation_6
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter no ");
+            int n = int.Parse(Console.ReadLine());
+            if (n != 0)
+            {
+                Console.WriteLine("enter second no");
+                int n1=int.Parse(Console.ReadLine());
+                Console.WriteLine(n*n1);
+            }
+            else
+                Console.WriteLine("0");
+            Console.ReadKey();
+        }
+        }
+}
+
