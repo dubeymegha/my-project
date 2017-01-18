42f57e72c76d4bb0ead38f4d8f36fb3be13537a8
megha dubey <dubeymegha14@gmail.com>
2017-01-18 14:26:38 -0800

    Devision program

---
+++ Devision_program_18.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class Devision_program_18
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<10;i++)
+            {
+            Console.WriteLine("number1 ?");
+            int n1=int.Parse(Console.ReadLine());
+            Console.WriteLine("number2 ?");
+            int n2=int.Parse(Console.ReadLine());
+
+            if (n1==0)
+            {
+                Console.WriteLine("Bye");
+                break;
+            }
+            if(n2==0)
+            {
+                Console.WriteLine("cannot devide by 0");
+            }
+                if(n1!=0 && n2!=0)
+                {
+                    Console.WriteLine("Devision is "+n1/n2);
+                    Console.WriteLine("Reaminder is "+n1%n2);
+                }
+        }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ login_pasword_17.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class login_pasword_17
+    {
+        static void Main(string[] args)
+        {
+            bool check = false;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter login");
+                int login = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter pass");
+                int password = int.Parse(Console.ReadLine());
+                while(login==12 && password==1234)
+                {
+                    Console.WriteLine("Login Successful");
+                    check = true;
+                    break;
+
+                }
+                if (check == true)
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
