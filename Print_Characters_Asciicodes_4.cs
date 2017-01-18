15d1ebffa689c7b2d825fca42f0360e62ca258be
megha dubey <dubeymegha14@gmail.com>
2017-01-18 12:24:28 -0800

    print ascii codes from 1 to 122

---
+++ Print_Characters_Asciicodes_4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class Print_Characters_Asciicodes_4
+    {
+        static void Main4(string[] args)
+        {
+            for(int i=1;i<=122;i++)
+            {
+                Console.WriteLine((char)i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
