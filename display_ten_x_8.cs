5b2f262bcf1bf71779ebaba4cdc59e78898b33fd
megha dubey <dubeymegha14@gmail.com>
2017-01-18 12:44:48 -0800

    display 10 times of the number untill no is zero

---
+++ display_ten_x_8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class display_ten_x_8
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter no");
+            int n = int.Parse(Console.ReadLine());
+            while(n!=0)
+            {
+                Console.WriteLine(10*n);
+                Console.WriteLine("enter no");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.ReadKey();
+        }
+    }
+}
