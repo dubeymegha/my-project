b4a44c7b18c8b1a56683bb367ca98a9a734ec452
megha dubey <dubeymegha14@gmail.com>
2017-01-18 13:55:57 -0800

    Display multiples of 3 and 5 from 1 to 500

---
+++ Multiplesof_3and5_14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class Multiplesof_3and5_14
+    {
+        static void Main(string[] args)
+        { 
+         for(int i=1;i<=500;i++)
+         {
+             if(i%3==0 && i%5==0 )
+             {
+                 Console.WriteLine(i);
+             }
+         }
+         Console.ReadKey();
+        }
+    }
+}
