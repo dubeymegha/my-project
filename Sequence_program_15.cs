f9a611769424813be0bfd20dee3ce809fc99dd91
megha dubey <dubeymegha14@gmail.com>
2017-01-18 14:02:51 -0800

    Sequence simple program

---
+++ Sequence_program_15.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class Sequence_program_15
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter no.");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter amount");
+            int a = int.Parse(Console.ReadLine());
+            for(int i=0;i<a;i++)
+            {
+                Console.Write(n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
