0128774c1bb137150e2228ea642d264c05a6665c
megha dubey <dubeymegha14@gmail.com>
2017-01-18 12:49:47 -0800

    print nos using while loop

---
+++ print_nos_using_while_9.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_practice_problems
+{
+    class print_nos_using_while_9
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while(n<=10)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
